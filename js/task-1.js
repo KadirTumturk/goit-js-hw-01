69f33f2786de259070eea044b43018506194cbcf
kadir_tumturk <kdrtumturk98@gmail.com>
2026-04-01 22:01:29 +0300

    task-01 update

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits. Delivery (${quantity} droids) is free of charge!`;
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
