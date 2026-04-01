98d88e6102f84f361f9b32203e60f02685246ccf
kadir_tumturk <kdrtumturk98@gmail.com>
2026-04-01 21:52:17 +0300

    task-3 update-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,11 @@
 function getElementWidth(content, padding, border) {
-    const contentNum = parseFloat(content);
-    const paddingNum = parseFloat(padding);
-    const borderNum = parseFloat(border);
-    
-    return contentNum + (paddingNum * 2) + (borderNum * 2);
+  const contentValue = parseFloat(content);
+  const paddingValue = parseFloat(padding);
+  const borderValue = parseFloat(border);
+
+  return `${contentValue + paddingValue * 2 + borderValue * 2}`;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-
